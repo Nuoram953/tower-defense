8b22697151d777470808a609317adb8facc8b126
MrRadish99 <69933825+MrRadish99@users.noreply.github.com>
2020-09-30 12:47:08 -0400

    Menu et jeu

--- TowerDefense.py
+++ TowerDefense.py
@@ -6,15 +6,75 @@
 # Antoine Auger
 # Mikael Korvat
 
-#TEST
-
 from tkinter import *
 import random 
+
 
 class Vue():  
     def __init__(self, modele, parent):
         self.modele = modele
         self.parent = parent
+        self.root = Tk()
+        self.root.title("towerDefense")
+        self.root.resizable(width = False, height = False)
+        self.windowMenu()
+        self.gameCanvas = None
+
+  
+    def windowMenu(self):
+      
+        self.menuFrame=Frame(self.root, bg = "white")
+
+        welcomeLabel = Label(self.root, text="WELCOME TO BOTANIK PANIK", pady = 20)
+
+        welcomeLabel.pack()
+
+        buttonNewGame = Button(self.menuFrame, text="Nouvelle Partie", command = self.newGame)
+        buttonOptions = Button(self.menuFrame,text="Options", command  = self.options)
+        buttonQuit = Button(self.menuFrame, text="Quitter", command = self.quit)
+
+        buttonNewGame.grid(column=0, row = 0, padx = 200, pady = 20)
+        buttonOptions.grid(column=0, row = 1, padx = 200, pady = 20)
+        buttonQuit.grid(column=0, row = 2, padx = 200, pady = 20) 
+
+        self.menuFrame.pack()
+
+    def newGame(self):
+        self.game = Tk()
+        self.game.resizable(width = False, height = False)
+        self.root.destroy()
+
+        self.gameFrame = Frame(self.game , width  = 1800, height = 1200)
+        self.gameCanvas=Canvas(self.gameFrame, width = 1400, height = 1200)
+        self.frameHUD = Frame(self.gameFrame, width = 400, height = 1200) 
+        
+        self.ressourceFrame=Frame(self.frameHUD, bg = "blue", width = 400, height = 400)
+        self.towerFrame=Frame(self.frameHUD, bg = "red", width = 400, height = 400)
+        self.upgradeFrame=Frame(self.frameHUD, bg = "black", width = 400, height = 400)
+
+        self.gameCanvas.grid(column = 0, row = 0)
+        self.frameHUD.grid(column = 1, row = 0)
+
+        self.ressourceFrame.grid(column = 1, row = 0)
+        self.towerFrame.grid(column = 1, row = 1)
+        self.upgradeFrame.grid(column = 1, row = 2)
+
+
+        self.gameFrame.pack(expand = YES, fill = BOTH)
+
+        self.img  = PhotoImage(file = "background.jpg")
+        self.gameCanvas.create_image(1400,1200, image = self.img, anchor = NW)
+    
+
+    def options(self):
+        pass
+
+    def quit(self):
+        pass    
+
+
+
+
 
 
 class Modele():  
@@ -26,8 +86,7 @@ class Controleur():
     def __init__(self):
         self.modele = Modele(self)
         self.vue = Vue(self,self.modele)
+        self.vue.root.mainloop()
 
 if __name__ == '__main__':
     c = Controleur()  
-
-     
