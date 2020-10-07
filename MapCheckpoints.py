dc04cb9bfce5573a9d7a2f5ff25c0ea61c9b239d
Caroline Emond Serret <caroline.emond.serret@gmail.com>
2020-10-07 12:17:00 -0400

    Ajout dictionnaire map checkpoints

---
+++ MapCheckpoints.py
@@ -0,0 +1,9 @@
+from TowerDefense import Checkpoint
+
+map = {
+    1: [Checkpoint(350,630), Checkpoint(350,380), Checkpoint(1150,380), Checkpoint(1150,655), Checkpoint(1400,655)],
+    2: [],
+    3: [],
+    4: [],
+    5: []
+}
