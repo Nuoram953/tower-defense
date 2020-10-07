f8a8557009442594f8e86aa4680c05c74660362d
Nuoram <antoinemaroun@gmail.com>
2020-10-07 12:11:56 -0400

    Added score and getProfil function in Modele

--- TowerDefense.py
+++ TowerDefense.py
@@ -9,6 +9,7 @@
 
 from tkinter import *
 import random
+from _datetime import date
 
 #test
 
@@ -100,7 +101,7 @@ class Creep1():
         self.buffer = 5
         self.height = 100
         self.width = 90
-        self.listImage = ["assets/zombies/zombie1.png", "assets/zombies/zombie2.png", "assets/zombies/zombie3.png","assets/zombies/zombie4.png","assets/zombies/zombie5.png","assets/zombies/zombie6.png"]
+        self.listImage = ["assets/zombies/zombie1.png", "assets/zombies/zombie2.png"]
         self.zombie = PhotoImage(file=random.choice(self.listImage))
         self.reachedEnd = False
 
@@ -265,6 +266,43 @@ class Modele():
                 del i
                 
             #if shot  
+
+
+    #Vérification si le joueur existe dans le fichier score.csv
+    #TODO: Changer le nom des variables et mettre en argument le nom du joueur
+    #TODO: Offrir au joueur la possibilité de refuser de continuer avec son profil si il veux recommencer?
+    def getProfil(self, namePlayer):
+        playerFound = False
+        file = open("score.csv", "r")
+        line=file.readlines()
+        print(line)
+        for player in line:
+            currentPlayer = player.split(",")
+            if currentPlayer[0].lower() == namePlayer.lower() and not playerFound:
+                playerFound = True
+                print("YEAH")
+                self.nom = currentPlayer[0]
+                self.niveau = currentPlayer[3]
+                self.sagesse = currentPlayer[4]
+        
+        if not playerFound:
+            self.nom = 0
+            self.niveau = 0
+            self.sagesse = 0
+
+        file.close()
+
+
+    #TODO: Changer le nom des variables
+    def score(self):
+        file = open("score.csv", "a")
+        file.write(f"{self.nom},{str(date.today())},{str(self.point)},{str(self.level)},{str(self.sagesse)}")
+        file.close()
+
+    
+                
+            
+
     
 
 class Controleur():
